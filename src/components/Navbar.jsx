8e8cd57787f3815a129b9baa44c4a51375ee7c3f
Tides <huythaislna@gmail.com>
2022-07-25 20:54:49 +0700

    init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div className='h-24 bg-violet-500 text-white text-center leading-[6rem] text-3xl'>
     <i className="fa fa-spotify mr-5">
     </i>
-    Spotify
+    Spotides
     </div>
     </>
   )
